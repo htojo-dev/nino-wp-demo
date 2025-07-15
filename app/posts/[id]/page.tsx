c68b966f73fe0c0378b81d5df81789b3343ee04f
htojo-dev <web.h.tojo1209@gmail.com>
2025-07-16 04:02:24 +0900

    feat(page): 記事詳細画面追加

---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { getPost } from "./data";
+
+export default async function Page({params: { id } }: { params: { id: string } }) {
+  const post = await getPost(id);
+  return (
+    <div>
+      <h1>{post.title.rendered}</h1>
+      <div dangerouslySetInnerHTML={{__html: post.content.rendered}}></div>
+    </div>
+  )
+}
--- app/posts/data.ts
+++ app/posts/data.ts
@@ -1,6 +1,7 @@
+import 'server-only';
+
 import { POST_ENDPOINT_URL } from '@/lib/wp';
 import { Post } from '@/types/post';
-import 'server-only';
 
 export const getPosts = async () => {
   const res = await fetch(POST_ENDPOINT_URL);
