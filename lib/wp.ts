7784c6edad36549d429510232446d081f2d9f97c
htojo-dev <web.h.tojo1209@gmail.com>
2025-07-15 18:08:05 +0900

    feat(lib): WordPress Endpoint用ヘルパー追加

---
+++ lib/wp.ts
@@ -0,0 +1,4 @@
+const API_BAE_URL = 'https://public-api.wordpress.com/wp/v2';
+const SITE_URL = 'htojocompany.wordpress.com';
+
+export const POST_ENDPOINT_URL = `${API_BAE_URL}/sites/${SITE_URL}/posts`;
