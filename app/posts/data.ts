a32f5062744a858f04f2e552daff9eb67475ae4d
htojo-dev <web.h.tojo1209@gmail.com>
2025-07-17 17:51:26 +0900

    feat(search): 検索画面追加

--- app/posts/data.ts
+++ app/posts/data.ts
@@ -3,7 +3,15 @@ import 'server-only';
 import { POST_ENDPOINT_URL } from '@/lib/wp';
 import { Post } from '@/types/post';
 
-export const getPosts = async () => {
-  const res = await fetch(POST_ENDPOINT_URL);
-  return await res.json() as Post[];
+export const searchPosts = async (keyword: string) => {
+  const res = await fetch(POST_ENDPOINT_URL + `?search=${keyword}`, {
+    cache: 'no-store',
+  });
+
+  const totalPageCount = (res.headers.get('x-wp-totalpages'));
+
+  return {
+    posts: (await res.json()) as Post[],
+    totalPageCount: Number(totalPageCount),
+  };
 }
